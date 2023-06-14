5a329d5093c927f6c92c24ece56857c3242aecfc
avivavraham <avivavraham@mail.tau.ac.il>
2023-06-14 21:19:55 +0300

    made HTTP TSP request from AzureMap Service successfully!

--- __init__.py
+++ __init__.py
@@ -1,116 +1,25 @@
-import os
 import requests
-from azure.core.credentials import AzureKeyCredential
-from azure.maps.route import MapsRouteClient
-# TODO: Put the relevant subscription key below from AzureMap Portal.
+# Put the relevant subscription key below from your AzureMap Service.
 AZURE_SUBSCRIPTION_KEY = "z3m2QmnGOqCnBPODEjxdCoiI2XzC-TaRgenvLd6AkzE"
 
-"""
-FILE: sample_get_route_range.py
-DESCRIPTION:
-    This sample demonstrates how to perform get route range with given lat/lon.
-USAGE:
-    python sample_get_route_range.py
 
-    Set the environment variables with your own values before running the sample:
-    - AZURE_SUBSCRIPTION_KEY - your subscription key
-"""
-
-subscription_key = os.getenv("AZURE_SUBSCRIPTION_KEY")
-
-
-def get_route_range():
-    # [START get_route_range]
-    from azure.core.credentials import AzureKeyCredential
-    from azure.maps.route import MapsRouteClient
-
-    # Set the Azure Subscription Key
-    os.environ["AZURE_SUBSCRIPTION_KEY"] = AZURE_SUBSCRIPTION_KEY
-
-    # Create the AzureKeyCredential object
-    credential = AzureKeyCredential(os.environ["AZURE_SUBSCRIPTION_KEY"])
-
-    # Create an instance of the MapsRouteClient
-    maps_route_client = MapsRouteClient(credential=credential)
-
-    result = maps_route_client.get_route_range(coordinates=(52.50931,13.42936), time_budget_in_sec=6000)
-
-    print("Get Route Range with coordinates and time budget:")
-    print(result.reachable_range.center)
-    print(result.reachable_range.boundary[0])
-    # [END get_route_range]
-
-"""
-def calculate_shortest_path(addresses):
-    # Set the Azure Subscription Key
-    os.environ["AZURE_SUBSCRIPTION_KEY"] = AZURE_SUBSCRIPTION_KEY
-
-    # Create the AzureKeyCredential object
-    credential = AzureKeyCredential(os.environ["AZURE_SUBSCRIPTION_KEY"])
-
-    # Create an instance of the MapsRouteClient
-    route_client = MapsRouteClient(credential=credential)
-
-    # Create a list of waypoints from the addresses
-    waypoints = [{"address": address} for address in addresses]
-
-    # Set the route calculation options
-    route_request = RouteDirectionsRequest(
-        waypoints=waypoints,
-        route_mode=RouteMode.fastest,
-        optimize=RouteDirectionsRequest.OptimizeOptions.distance,
-    )
-    # Call the route directions API to get the optimized route
-    response = route_client.calculate_directions(route_request)
-
-    if response.error:
-        print("Error:", response.error.message)
-        return None
-
-    # Extract the route information
-    route = response.routes[0]
-    optimized_route = route.legs[0].points
-
-    # Extract the coordinates of the optimized route
-    coordinates = [(point.latitude, point.longitude) for point in optimized_route]
-
-    return coordinates
-
-
-# Example usage
-addresses_of_dustbins = [
-    "Address 1",
-    "Address 2",
-    "Address 3",
-    # Add more addresses here...
-]
-
-path = calculate_shortest_path(addresses_of_dustbins)
-if path:
-    print("Optimized Path:")
-    for lat, lon in path:
-        print(f"Latitude: {lat}, Longitude: {lon}")
-else:
-    print("Failed to calculate the path.")
-"""
-
-
-
-
-
-def calculate_route_directions(origin, destination):
+def optimize_route_waypoints(waypoints):
     # Azure Maps Route Directions API endpoint
+    global response
     endpoint = "https://atlas.microsoft.com/route/directions/json"
 
     # Azure Maps API key
     api_key = AZURE_SUBSCRIPTION_KEY
 
+    # Format the query string with waypoints
+    query = ":".join(waypoints)
+
     # Query parameters
     params = {
         "api-version": "1.0",
-        "query": f"{origin}:{destination}",
-        "report": "effectiveSettings",
-        "subscription-key": api_key
+        "subscription-key": api_key,
+        "computeBestOrder": "true",
+        "query": query
     }
 
     try:
@@ -126,8 +35,15 @@ def calculate_route_directions(origin, destination):
             route = data["routes"][0]
             points = route["legs"][0]["points"]
             coordinates = [(point["latitude"], point["longitude"]) for point in points]
-
-            return coordinates
+            # The optimized waypoint order information from the Routing service provides a set of indices.
+            # These exclude the origin and the destination indices.
+            # so we need to increment these values by 1 to account for the origin.
+            optimized_route = [node["optimizedIndex"] + 1 for node in data["optimizedWaypoints"]]
+            # here I returned Indices, and ignored the route field in data.
+            # If needed, you can return route information from the route field.
+            # look for https://learn.microsoft.com/en-us/azure/azure-maps/how-to-use-best-practices-for-routing for
+            # additional information.
+            return optimized_route
         else:
             print("No routes found.")
             return None
@@ -137,14 +53,27 @@ def calculate_route_directions(origin, destination):
         print(f"Error message: {error}")
         return None
 
-# Example usage
-origin = "52.50931,13.42936"
-destination = "52.50274,13.43872"
 
-path = calculate_route_directions(origin, destination)
+# Example usage, Here the first and last are equal to perform a cyclic route.
+waypoints = [
+    "47.606544,-122.336502",
+    "47.759892,-122.204821",
+    "47.670682,-122.120415",
+    "47.480133,-122.213369",
+    "47.615556,-122.193689",
+    "47.676508,-122.206054",
+    "47.495472,-122.360861",
+    "47.606544,-122.336502"
+]
+
+# The optimized waypoint order information from the Routing service provides a set of indices.
+# These exclude the origin and the destination indices.
+# so we need to increment these values by 1 to account for the origin.
+path = [0] + optimize_route_waypoints(waypoints) + [0]
+print(path)
 if path:
     print("Optimized Path:")
-    for lat, lon in path:
-        print(f"Latitude: {lat}, Longitude: {lon}")
+    for index in path:
+        print(f"Node: {index}, Latitude: {waypoints[index].split(',')[0]}, Longitude: {waypoints[index].split(',')[1]}")
 else:
     print("Failed to calculate the path.")
